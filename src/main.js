7082c42614ebce6f7cde04cfb0e3e3466f216a8d
cris <c.cariqueo03@ufromail.cl>
2024-10-13 23:17:51 -0300

    agregada funcion para determinar titulo de pagina
    declarar en 'name' al crear ruta en router

--- src/main.js
+++ src/main.js
@@ -11,3 +11,8 @@ import App from './App.vue'
 import router from '../router.js';  // import router from './../router';
 
 createApp(App).use(router).mount('#app')
+
+router.beforeEach((to, from, next) => {
+    document.title = to.name;
+    next();
+  });
